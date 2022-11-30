612e792d0062cda639551d83a38a72ecfb555a8c
Sean Killeen <SeanKilleen@gmail.com>
2022-11-30 12:17:21 -0500

    Failing test case

--- StringCalcKata/Add-Numbers.Tests.ps1
+++ StringCalcKata/Add-Numbers.Tests.ps1
@@ -6,4 +6,13 @@ Describe "Add-Numbers" {
     It "Returns zero when string is empty" {
         Add-Numbers "" | Should -Be 0
     }
+
+    It "Returns a single value when passed in (<input>: <expected>)" -Foreach @(
+        @{Input = "0"; Expected = 0 }
+        @{Input = "1"; Expected = 1 }
+        @{Input = "123"; Expected = 123 }
+    ) {
+        Add-Numbers $input | Should -Be $expected
+    }
 }
+
